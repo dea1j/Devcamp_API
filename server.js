e1b192c52487e7cc0b50b0d529980e47e15c3c1d
dea1j <jdayo2012@gmail.com>
2020-06-17 00:32:27 +0100

    Updated

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
+// const logger = require('./middleware/logger');
+const morgan = require('morgan');
+
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// app.use(logger);
+
+// Dev logging environment
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
